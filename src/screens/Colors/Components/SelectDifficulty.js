924f5937f39926da5c9355f195d9d7af43735b63
luiz valdameri <luiz.valdameri@ciss.com.br>
2021-08-10 13:24:42 -0300

    Added first screen and some functions

--- src/screens/Colors/Components/SelectDifficulty.js
+++ src/screens/Colors/Components/SelectDifficulty.js
@@ -1,20 +1,20 @@
 import { Box, Button, Typography } from '@material-ui/core'
 import React from 'react'
 
-export default function SelectDifficulty() {
+export default function SelectDifficulty(props) {
     return (
         <Box style={{ height: '600px', width: "400px" }} flexDirection="column" display="flex" alignItems="center" justifyContent="center">
             <Typography variant="h4">
                 Select Your difficulty
             </Typography>
             <Box maxWidth display="flex" alignItems="center" justifyContent="space-between" style={{width: "100%", padding: "20px 50px"}}> 
-                <Button variant="contained" color="primary" >
+                <Button variant="contained" color="primary" onClick={() => props.changeDifficulty("easy")}>
                     Easy
                 </Button>
-                <Button variant="contained" color="primary">
+                <Button variant="contained" color="primary" onClick={() => props.changeDifficulty("medium")}>
                     Medium
                 </Button>
-                <Button variant="contained" color="primary">
+                <Button variant="contained" color="primary" onClick={() => props.changeDifficulty("hard")}>
                     Hard
                 </Button>
             </Box>
--- src/screens/Colors/index.js
+++ src/screens/Colors/index.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
 import SelectDifficulty from './Components/SelectDifficulty'
+import SelectColors from './Components/SelectColors'
+import ShowColors from './Components/ShowColors'
+import Service from "../../helpers/Service"
 
 export class Colors extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            step: 0,
+            difficulty: null,
+            colors: null
+        }
+    }
+
+    changeStep = (value) => {
+        this.setState({ step: value })
+    }
+
+    changeDifficulty = (value) => {
+        this.setState({ difficulty: value })
+        this.getColors();
+    }
+
+    teste = () => {
+        console.log(this.state.step);
+        this.changeState("step", 0);
+        console.log(this.state.step);
+    }
+
+    getColors = () => {
+        var service = new Service();
+
+        service
+            .get("/colors")
+            .then(res => {
+                this.setState({ colors: res.colors, step: 1 })
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
+    changeScreen = () => {
+        const { step } = this.state;
+
+        switch (step) {
+            case 0:
+                return <SelectDifficulty changeDifficulty={this.changeDifficulty} />
+            case 1:
+                return <ShowColors changeDifficulty={this.changeDifficulty} />
+            case 2:
+                return <SelectColors changeDifficulty={this.changeDifficulty} />
+            default:
+                return <SelectDifficulty changeDifficulty={this.changeDifficulty} />
+        }
+
+    }
+
     render() {
         return (
             <React.Fragment>
-                <SelectDifficulty />
+                {this.changeScreen()}
             </React.Fragment>
         )
     }
